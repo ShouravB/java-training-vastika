1f7ed678190d0210c506b6227d784f958cbe49e4
ShouravB <shourav.bhattarai@gmail.com>
2023-09-18 12:26:30 -0700

    Date assignment finding posts duration

---
+++ src/predefinedclasses/DateClassExamples.java
@@ -0,0 +1,21 @@
+package predefinedclasses;
+
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.chrono.ChronoLocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+public class DateClassExamples {
+    public static void main(String[] args) {
+
+
+
+        /*
+            Date format to use yyyy-MM-ddTHH:mm:ss
+          */
+        DateUtils.findPostDuration("2024-07-18T12:12:39");
+//        System.out.println(postedDate.until(currentDate, ChronoUnit.MILLIS));
+//        System.out.println(postedDate);
+    }
+}
---
+++ src/predefinedclasses/DateUtils.java
@@ -0,0 +1,38 @@
+package predefinedclasses;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+public final class DateUtils {
+
+    private  DateUtils(){}
+
+    public static void findPostDuration (String postDate){
+
+        LocalDateTime currentDate= LocalDateTime.now();
+        LocalDateTime postedDate = LocalDateTime.parse(postDate);
+        if(currentDate.isBefore(postedDate)){
+            System.out.println("The post is from the future");
+        }else if(postedDate.equals(currentDate)){
+            System.out.println("Just Now");
+        } else if (postedDate.until(currentDate, ChronoUnit.YEARS) != 0) {
+            System.out.println(postedDate.until(currentDate,ChronoUnit.YEARS) + " years ago");
+        }else if (postedDate.until(currentDate, ChronoUnit.MONTHS) != 0) {
+            System.out.println(postedDate.until(currentDate,ChronoUnit.MONTHS) + " months ago");
+        }else if (postedDate.until(currentDate, ChronoUnit.DAYS) != 0) {
+            System.out.println(postedDate.until(currentDate,ChronoUnit.DAYS) + " days ago");
+        }else if (postedDate.until(currentDate, ChronoUnit.HOURS) != 0) {
+            System.out.println(postedDate.until(currentDate,ChronoUnit.HOURS) + " hours ago");
+        }else if (postedDate.until(currentDate, ChronoUnit.MINUTES) != 0) {
+            System.out.println(postedDate.until(currentDate,ChronoUnit.MINUTES) + " minutes ago");
+        }else if (postedDate.until(currentDate, ChronoUnit.SECONDS) != 0) {
+            System.out.println(postedDate.until(currentDate,ChronoUnit.SECONDS) + " seconds ago");
+        }else {
+            System.out.println("Let me check the duration");
+        }
+
+    }
+}
