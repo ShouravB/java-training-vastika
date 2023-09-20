a3efc1d842bb7420e05de6f914d75ced8be5952a
ShouravB <shourav.bhattarai@gmail.com>
2023-09-19 20:43:11 -0700

    updated DateUtils to handle exception

--- src/predefinedclasses/DateClassExamples.java
+++ src/predefinedclasses/DateClassExamples.java
@@ -8,7 +8,9 @@ public class DateClassExamples {
         /*
             Date format to use yyyy-MM-ddTHH:mm:ss
           */
-        DateUtils.calculatePostDuration("2024-07-18T12:12:39");
+        DateUtils.calculatePostDuration("2024/07/18T12:12:39");
+        System.out.println("*****************************************************************");
+        DateUtils.calculatePostDuration("2022-07-18T12:12:39");
 //        System.out.println(postedDate.until(currentDate, ChronoUnit.MILLIS));
 //        System.out.println(postedDate);
     }
--- src/predefinedclasses/DateUtils.java
+++ src/predefinedclasses/DateUtils.java
@@ -1,5 +1,7 @@
 package predefinedclasses;
 
+import java.text.ParseException;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
@@ -9,27 +11,34 @@ public final class DateUtils {
 
     public static void calculatePostDuration(String postDate){
 
-        LocalDateTime currentDate= LocalDateTime.now();
-        LocalDateTime postedDate = LocalDateTime.parse(postDate);
-        if(currentDate.isBefore(postedDate)){
-            System.out.println("The post is from the future");
-        }else if(postedDate.equals(currentDate)){
-            System.out.println("Just Now");
-        } else if (postedDate.until(currentDate, ChronoUnit.YEARS) != 0) {
-            System.out.println(postedDate.until(currentDate,ChronoUnit.YEARS) + " years ago");
-        }else if (postedDate.until(currentDate, ChronoUnit.MONTHS) != 0) {
-            System.out.println(postedDate.until(currentDate,ChronoUnit.MONTHS) + " months ago");
-        }else if (postedDate.until(currentDate, ChronoUnit.DAYS) != 0) {
-            System.out.println(postedDate.until(currentDate,ChronoUnit.DAYS) + " days ago");
-        }else if (postedDate.until(currentDate, ChronoUnit.HOURS) != 0) {
-            System.out.println(postedDate.until(currentDate,ChronoUnit.HOURS) + " hours ago");
-        }else if (postedDate.until(currentDate, ChronoUnit.MINUTES) != 0) {
-            System.out.println(postedDate.until(currentDate,ChronoUnit.MINUTES) + " minutes ago");
-        }else if (postedDate.until(currentDate, ChronoUnit.SECONDS) != 0) {
-            System.out.println(postedDate.until(currentDate,ChronoUnit.SECONDS) + " seconds ago");
-        }else {
-            System.out.println("Let me check the duration");
+        try {
+            LocalDateTime currentDate = LocalDateTime.now();
+            LocalDateTime postedDate = LocalDateTime.parse(postDate);
+            if (currentDate.isBefore(postedDate)) {
+                System.out.println("The post is from the future");
+            } else if (postedDate.equals(currentDate)) {
+                System.out.println("Just Now");
+            } else if (postedDate.until(currentDate, ChronoUnit.YEARS) != 0) {
+                System.out.println(postedDate.until(currentDate, ChronoUnit.YEARS) + " years ago");
+            } else if (postedDate.until(currentDate, ChronoUnit.MONTHS) != 0) {
+                System.out.println(postedDate.until(currentDate, ChronoUnit.MONTHS) + " months ago");
+            } else if (postedDate.until(currentDate, ChronoUnit.DAYS) != 0) {
+                System.out.println(postedDate.until(currentDate, ChronoUnit.DAYS) + " days ago");
+            } else if (postedDate.until(currentDate, ChronoUnit.HOURS) != 0) {
+                System.out.println(postedDate.until(currentDate, ChronoUnit.HOURS) + " hours ago");
+            } else if (postedDate.until(currentDate, ChronoUnit.MINUTES) != 0) {
+                System.out.println(postedDate.until(currentDate, ChronoUnit.MINUTES) + " minutes ago");
+            } else if (postedDate.until(currentDate, ChronoUnit.SECONDS) != 0) {
+                System.out.println(postedDate.until(currentDate, ChronoUnit.SECONDS) + " seconds ago");
+            } else {
+                System.out.println("Let me check the duration");
+            }
+        }catch(Exception e){
+            //e.printStackTrace();
+            System.out.println("Invalid date format. Please enter date in format of yyyy-MM-ddTHH:mm:ss");
         }
+
+        //System.out.println(Duration.between(postedDate,currentDate));
 
     }
 }
