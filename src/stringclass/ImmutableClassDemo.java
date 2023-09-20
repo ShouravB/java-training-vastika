bf8416e4c21ac67a8b3dbb306028fa42d7b8e4b8
ShouravB <shourav.bhattarai@gmail.com>
2023-09-20 10:19:16 -0700

    immutable class theory added

--- src/stringclass/ImmutableClassDemo.java
+++ src/stringclass/ImmutableClassDemo.java
@@ -2,6 +2,22 @@ package stringclass;
 
 public class ImmutableClassDemo {
     public static void main(String[] args) {
+
+        /*
+        Immutable class in Java means that once an object is created for that class, we cannot
+        change the content. All the wrapper classes are immutable.
+
+         */
+
+        /*
+        Steps to create immutable class
+        1. Declare the class final.
+        2. All members should be declared private and final.
+        3. Create a parameterized constructor to initialize all fields.
+         */
+
+
+
         CustomImmutableClass object = new CustomImmutableClass("Taman",32);
         System.out.println(object.getName());
         System.out.println(object.getAge());
