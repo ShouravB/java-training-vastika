b53076528e709297eacabb91ec502a84566a9f7e
ShouravB <shourav.bhattarai@gmail.com>
2023-09-25 14:16:05 -0700

    String assignment 2 added

---
+++ src/assignments/StringSplitExample.java
@@ -0,0 +1,30 @@
+package assignments;
+
+import static java.lang.Integer.parseInt;
+
+public class StringSplitExample {
+
+    public static void main(String[] args) {
+
+        try{
+            String input ="5 12";
+            String[] splitInput = input.split(" ");
+            StringBuilder outputString=new StringBuilder();
+            int count =0;
+
+
+            for (int i = parseInt(splitInput[0]); i< parseInt(splitInput[splitInput.length-1]); i++){
+                if(i % 5 == 0){
+                    outputString.append(i);
+                    outputString.append(" ");
+                    count++;
+                }
+            }
+
+            System.out.println(outputString);
+        }catch(Exception e){
+            System.out.println("Something went wrong. Please check the inputs");
+        }
+
+    }
+}
